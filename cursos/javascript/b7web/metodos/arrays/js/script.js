24f0d6cad02373effe720704cbbd9e52cad5fa42
Gustavo <gustavo.olliveira49@gmail.com>
2025-03-18 15:42:29 -0300

    classe date e métodos em js

--- cursos/javascript/b7web/metodos/arrays/js/script.js
+++ cursos/javascript/b7web/metodos/arrays/js/script.js
@@ -9,6 +9,10 @@ console.log(paraString2);
 let posicaoString = lista.indexOf('Corante');
 console.log(posicaoString);
 
+
+/* */ console.log("") /* */
+
+
 lista.pop();
 let listaSemUltimo = lista;
 console.log(listaSemUltimo);
@@ -34,3 +38,48 @@ listasJuntas.reverse();
 console.log(listasJuntas);
 
 
+/* */ console.log(""); /* */
+
+
+let lista3 = [45, 4, 9, 16, 25];
+let lista4 = [];
+
+lista4 = lista3.map(function(item) {
+    return item * 2;
+});
+console.log(lista4);
+
+lista4 = lista4.filter(function(item) {
+    if(item < 20){
+        return true;
+    }else{
+        return false;
+    }
+});
+console.log(lista4);
+
+lista3 = lista3.every(function(item) {
+    return (item > 20)? true : false;
+});
+console.log(lista3);
+
+lista3 = [45, 4, 9, 16, 25];
+lista4 = [];
+
+lista4 = lista3.some(function(item) {
+    return (item > 20)? true : false;
+});
+console.log(lista4);
+
+let lista5 = [74, 1, 47, 16, 12];
+let lista6 = [];
+
+lista6 = lista5.find(function(item) {
+    if(item == 16) return true;
+});
+console.log(lista6);
+
+lista6 = lista5.findIndex(function(item) {
+    if(item == 16) return true;
+});
+console.log(lista6);
---
+++ cursos/javascript/b7web/metodos/date/js/script.js
@@ -0,0 +1,13 @@
+let data = new Date();
+
+console.log(data);
+console.log(data.toDateString());
+
+let data2 = new Date(2020, 0, 1, 12, 30, 12);
+console.log(data2);
+
+let data3 = new Date('2020-01-01 15:42:15');
+console.log(data3);
+
+let data4 = new Date(0);
+console.log(data4);
