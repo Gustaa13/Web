80002a2c398c245a755b09e4d7e37ebc3670536f
Gustavo <gustavo.olliveira49@gmail.com>
2025-03-19 10:28:29 -0300

    métodos de datas e matemática em js

--- cursos/javascript/b7web/metodos/date/js/script.js
+++ cursos/javascript/b7web/metodos/date/js/script.js
@@ -11,3 +11,38 @@ console.log(data3);
 
 let data4 = new Date(0);
 console.log(data4);
+
+let mes = data.getMonth();
+console.log(mes);
+
+let dia = data.getDate();
+console.log(dia);
+
+let hora = data.getHours();
+console.log(hora);
+
+let minutos = data.getMinutes();
+console.log(minutos);
+
+let segundos = data.getSeconds();
+console.log(segundos);
+
+let miliSegundosAteHoje = data.getTime();
+console.log(miliSegundosAteHoje);
+
+let data5 = new Date();
+
+data5.setFullYear(2022);
+console.log(data5);
+
+data5.setMonth(11);
+console.log(data5);
+
+data5.setDate(1);
+console.log(data5);
+
+data5.setDate(data5.getDate() + 5);
+console.log(data5);
+
+data5.setHours(data5.getHours() + 20);
+console.log(data5);
---
+++ cursos/javascript/b7web/metodos/math/js/script.js
@@ -0,0 +1,23 @@
+let pi = Math.PI; // Constantes
+console.log(pi);
+
+let valorArredondado = Math.round(3.73);
+console.log(valorArredondado);
+
+let valorArredondadoParaBaixo = Math.floor(3.92);
+console.log(valorArredondadoParaBaixo);
+
+let valorArredondadoParaCima = Math.ceil(3.08);
+console.log(valorArredondadoParaCima);
+
+let valorAbsoluto = Math.abs(-42);
+console.log(valorAbsoluto);
+
+let menorValor = Math.min(2, 34, 566, 21);
+console.log(menorValor);
+
+let maiorValor = Math.max(2, 32, 566, 13);
+console.log(maiorValor);
+
+let valorAleatorio = Math.round(Math.random() * 50);
+console.log(valorAleatorio);
